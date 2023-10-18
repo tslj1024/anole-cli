028b5a336a22064878f734a6dee8fe4165868524
ma_yuan <ma_yuan163@163.com>
2023-10-18 21:39:41 +0800

    集成 vue-router （尚需动态路由）

---
+++ src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+    // eg:
+    // {
+    //     path: '/login',
+    //     name: 'Login',
+    //     meta: {
+    //         title: '登录',
+    //         keepAlive: true,
+    //         requireAuth: false
+    //     },
+    //     component: () => import('@/pages/login.vue')
+    // },
+    // {
+    //     path: '/',
+    //     name: 'Index',
+    //     meta: {
+    //         title: '首页',
+    //         keepAlive: true,
+    //         requireAuth: true
+    //     },
+    //     component: () => import('@/pages/index.vue')
+    // }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+export default router;
