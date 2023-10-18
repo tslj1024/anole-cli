28ae836b8194303b417c33cda65381c51b0e8a4f
ma_yuan <ma_yuan163@163.com>
2023-10-18 21:42:49 +0800

    集成 vueUse

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import VueUse from "@/pages/vueUse.vue";
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+    <vue-use />
   <router-view />
 </template>
 
