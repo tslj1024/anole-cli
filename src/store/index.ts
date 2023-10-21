db7630f3135d30e4a17741d7971d9ca11dae98ea
ma_yuan <ma_yuan163@163.com>
2023-10-21 17:52:07 +0800

    :package: build(全项目文件): pinia 持久化

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,9 @@
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 const store = createPinia();
+
+// pinia 持久化
+store.use(piniaPluginPersistedstate);
 
 export default store;
